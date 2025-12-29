8702878b580c4a83d92d114a871831a449f00a66
Hasaan <hasaanazeem07@gmail.com>
2025-12-29 18:15:40 +0500

    Changes saved

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -47,7 +47,7 @@ export default function Process() {
 
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8 relative">
           {/* Connecting Line Desktop */}
-          <div className="hidden md:block absolute top-12 left-0 w-full h-0.5 bg-gray-100 -z-0"></div>
+          <div className="hidden md:block absolute top-12 left-0 w-full h-0.5 bg-gray-100 z-0"></div>
 
           {steps.map((step, index) => {
             const Icon = step.icon;
